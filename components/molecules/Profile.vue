ad3d199d3608a73b83d23ad6d167f66735934a40
Alexandre Ferreira <alexmlf93@gmail.com>
2021-03-25 10:34:53 -0300

    CompletedChallenges and Profile components

---
+++ components/molecules/Profile.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="flex items-center">
+		<img src="https://github.com/magno193.png" alt="Alexandre Ferreira" class="mr-6 w-20 h-20 rounded-full">
+		<div class="flex flex-col w-full">
+			<strong class="text-title text-2xl">Alexandre Ferreira</strong>
+			<p class="flex items-center">
+				<img src="Icons/level.svg" alt="level" class="mr-2">
+				<span class="text-base">Level {{ level }}</span>
+			</p>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { mapState } from 'vuex';
+export default Vue.extend({
+	computed: mapState('Challenges', ['level']),
+});
+</script>
+
+<style>
+
+</style>
--- store/Challenges/index.ts
+++ store/Challenges/index.ts
@@ -2,13 +2,13 @@ import allChallenges from '~/assets/challenges/data';
 import { State } from './types';
 
 export const state = (): State => ({
-	level: 1,
+	level: 10,
 	xp: {
-		current: 0,
+		current: 30,
 		start: 0,
 		end: 64,
 	},
-	completedChallenges: 0,
+	completedChallenges: 5,
 	currentChallengeIndex: null,
 	isLevelUpModalOpen: false,
 	allChallenges,
