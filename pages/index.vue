9ace5783a3d38d640800af5de9f24f17bafc1462
Alexandre Ferreira <alexmlf93@gmail.com>
2021-03-25 21:33:29 -0300

    Countdown components and logic implemetation

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,42 @@
 		<div class="flex flex-col w-full lg:w-1/2">
 			<Profile />
 			<CompletedChallenges />
+			<Countdown @completed="getNewChallenge" />
+			<button
+				v-if="hasCountdownCompleted"
+				disabled
+				class="button completed"
+			>
+				Cycle completed
+			</button>
+			<button
+				v-else-if="isCountdownActive"
+				class="button abadon"
+				@click="setCountdownState(false)"
+			>
+				Abandon cycle
+			</button>
+			<button
+				v-else
+				class="button start"
+				@click="setCountdownState(true)"
+			>
+				Start a cycle
+			</button>
 		</div>
 	</section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import { mapState, mapMutations } from 'vuex';
+import { Mutations as CountdownMT } from '~/store/Countdown/types';
+
 import CompletedChallenges from '~/components/atoms/CompletedChallenges.vue';
 import Profile from '~/components/molecules/Profile.vue';
+import Countdown from '~/components/molecules/Countdown.vue';
+
+import { playAudio, sendNotification } from '~/utils';
 
 interface Head {
 	title: string;
@@ -25,6 +53,39 @@ export default Vue.extend({
 	components: {
 		CompletedChallenges,
 		Profile,
+		Countdown,
+	},
+	mounted () {
+		if ('Notification' in window) {
+			Notification.requestPermission();
+		}
+	},
+	computed: {
+		...mapState('Countdown', {
+			hasCountdownCompleted: 'hasCompleted',
+			isCountdownActive: 'isActive',
+		}),
+	},
+	methods: {
+		...mapMutations({
+			setCountdownHasCompleted: `Countdown/${CountdownMT.SET_HAS_COMPLETED}`,
+			setCountdownIsActive: `Countdown/${CountdownMT.SET_IS_ACTIVE}`,
+		}),
+		setCountdownState (flag:boolean) {
+			this.setCountdownHasCompleted(false);
+			this.setCountdownIsActive(flag);
+		},
+		getNewChallenge () {
+			this.setCountdownHasCompleted(true);
+
+			if (Notification?.permission === 'granted') {
+				playAudio('/static_notification.mp3');
+				sendNotification('New Challenge1', {
+					body: 'A new challenge has started! Go complete it!',
+					icon: '/favicon.png',
+				});
+			}
+		},
 	},
 });
 </script>
--- store/Countdown/index.ts
+++ store/Countdown/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable space-before-function-paren */
 import { State, Getters, Mutations, MutationsInterface } from './types';
 
-const MINUTES = 25;
+const MINUTES = 0.05;
 
 export const state = (): State => ({
 	time: MINUTES * 60,
